96c8a1644ed835c29201c9a80df9633377614780
Arabaleksey <aleksey-090@mail.ru>
2022-11-14 20:31:29 +0300

    [HW-TEST/Arabaleksey] - some text

--- HW-TEST/index.js
+++ HW-TEST/index.js
@@ -1,10 +1,17 @@
 // Написать функцию которая возвращает результат умножения 2-ух чиесл
-let multiply = function(){
-    let a = +prompt('Введите первое число');
-    let b = +prompt('Введите второе число');
-    return a*b
+let multiply = function(a,b){
+    let res = a*b
+    return res
 }
-let result = multiply();
+let result = multiply(2,3);
 console.log(result);
 
+
+// let multiply = function(){
+//     let a = +prompt('Введите первое число');
+//     let b = +prompt('Введите второе число');
+//     return a*b
+// }
+// let result = multiply();
+// console.log(result);
 
