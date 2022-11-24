81f372a71dad15282ad36af8f068a3ad5f21dd3d
davidjoe1x <david.zograbian@mail.ru>
2022-11-24 16:22:38 +0300

    hw5

---
+++ hw5.js
@@ -0,0 +1,53 @@
+// const colors = ['red', 'green', 'blue']
+// console.log(colors.length);
+
+
+// const animals = ['monkey', 'dog', 'cat']
+// console.log(animals[2]);
+
+
+// const numbers = [5, 43, 63, 23, 90]
+// numbers.splice(0, 5);
+// console.log(numbers);
+
+// const numbers = [5, 43, 63, 23, 90]
+// numbers.length = 0;
+// console.log(numbers);
+
+// const students = ['Polina', 'Dasha', 'Masha']
+// let LastItem = students.pop();
+// let FirstItem = students.shift();
+// students.push('Borya');
+// students.unshift('Andrey');
+// console.log(students);
+
+
+// const cats = ['Gachito', 'Tom', 'Batman']
+// for (let i = 0; i < cats.length; i++) {
+//     let item = cats[i];
+// }
+// console.log(cats);
+
+
+// const cats = ['Gachito', 'Tom', 'Batman']
+// for (let value of cats) {
+//     console.log(value);
+//   }
+
+
+// const evenNumbers = [2, 4, 6, 8, 10]
+// const oddNumbers = [1, 3, 5, 7, 9]
+// let merge = evenNumbers.concat(oddNumbers);
+// console.log(merge);
+// console.log(merge.indexOf(8, 0));
+
+
+// const binary = [0, 1, 0, 1, 0, 1, 0]
+// let str = binary.join('');
+
+// console.log(str);
+
+
+   const mixedNumbers = [-14, -89, -1]
+   const positiveNum = [24, 43, 0, 412, 4]
+   console.log(positiveNum, mixedNumbers);
