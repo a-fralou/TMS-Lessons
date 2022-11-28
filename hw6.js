1a6026d0d4c980d7b82572034f1d2d5c3dbd0526
davidjoe1x <david.zograbian@mail.ru>
2022-11-28 16:18:24 +0300

    hw6

---
+++ hw6.js
@@ -0,0 +1,44 @@
+
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+
+// function arrayShow(fibonacci) {
+//   fibonacci.forEach(element => console.log(element));
+// }
+// arrayShow(fibonacci);
+
+
+
+// const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton']
+// let result =  users.map(function(elem) {
+// 	return "member " + users.indexOf(elem) + ": " + elem;
+// }) 
+
+// let newArray = [result]
+// console.log(result);
+
+// const numbers = [7, -4, 32, -90, 54, 32, -21]
+
+// const result = numbers.filter(numbers => numbers > 0);
+
+// console.log(result);
+
+
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
+
+// const initialValue = 0;
+// const sumWithInitial = fibonacci.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue
+// );
+
+// console.log(sumWithInitial);
+
+
+
+// const numbers = [5, 9, 13, 24, 54, 10, 13, 99, 1, 5]
+
+// let result = numbers.find(function(elem) {
+// 	return elem % 2 == 0;
+// });
+
+// console.log(result);
