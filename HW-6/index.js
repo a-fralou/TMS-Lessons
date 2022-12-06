40d44be8ac6b84151098dea4b0cf6dba44f7c70b
Gorbatylya <dariagorbatykh2004@gmail.com>
2022-12-06 21:42:04 +0300

    b

--- HW-6/index.js
+++ HW-6/index.js
@@ -75,3 +75,68 @@
 // 2 вариант
 // chetn = numbers.find((i)=> i % 2 == 0)
 // console.log (chetn)
+
+
+// Advance
+
+// Task 2 👨‍🏫 Тролли атакуют наш раздел с комментариями!!!
+// Единственный способ справиться с этой ситуацией - удалить все гласные из комментариев троллей, нейтрализуя угрозу.
+// Ваша задача - написать функцию, которая принимает строку и возвращает новую строку с удаленными гласными.
+// Например, строка «This website is for losers LOL!» станет "Ths wbst s fr lsrs LL!".
+// Примечание: для этой задачи y не считается гласной.
+
+
+
+// function string (str){
+//   str = 'This website is for losers LOL!';
+//   let аrrVowels =['a', 'e', 'i', 'o', 'u'];
+//   let result = str.split('');
+//   console.log(result)
+
+//   for (let i = 0; i<result.length;i++){
+
+//   for(let n=0; n<аrrVowels.length; n++){
+//     if (result[i] === arrVowels[n]){
+//       str.delete(result[i])}
+//     }
+//   }
+// }
+
+// string()
+
+// function disemvowel(str) {
+//     str =str.replace(/[aeiou]/gi, '');   //что это значит
+//   return str;
+// }
+// let str = "This website is for losers LOL!";
+// console.log(disemvowel(str));
+
+
+// Task3
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+//Параметр - это строка, которая включает только буквы от a..z и A..Z.
+
+
+// function dubleLetter (string){
+//     string = 'RqaEzty';
+//     let arr = string.split('');
+//     for(let i=0; i<arr.length; i++){
+//       let a = arr[i].toUpperCase() + arr[i].repeat(i).toLowerCase()
+//       console.log(a)
+//     };
+//     return string
+// }
+
+// console.log(dubleLetter())
+
+let accum = str => str
+    .split('')
+    .map((ch, i) => (ch = ch.toLowerCase().repeat(i + 1)) && ch.charAt(0).toUpperCase() + ch.slice(1))
+    .join('-')
+
+    console.log(accum("abcd"));    // "A-Bb-Ccc-Dddd"
+    console.log(accum("RqaEzty")); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+    console.log(accum("cwAt"));
+
